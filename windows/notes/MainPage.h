ffae0ee1c9caf31f3370e127ac7b24fd18d44612
aargon007 <muhai4959@gmail.com>
2025-05-06 21:42:47 +0600

    project initialized

---
+++ windows/notes/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::notes::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::notes::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
